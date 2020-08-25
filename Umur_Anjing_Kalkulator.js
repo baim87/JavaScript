ce6f7430679127488592375efc3a49cc4c08fad0
Balya Malkanul Karim <balya.karim@gmail.com>
2020-08-25 21:25:45 +0700

    Create Umur_Anjing_Kalkulator.js

---
+++ Umur_Anjing_Kalkulator.js
@@ -0,0 +1,7 @@
+var dogAge = prompt('Berapa umur anjingmu?');
+var HumanAge = (dogAge - 2) * 4 + 21;
+if (HumanAge > 1) {
+alert('Your dog age is ' + HumanAge + ' years old.')
+}else{
+alert('Your dog age is ' + HumanAge + ' year old.')
+}
